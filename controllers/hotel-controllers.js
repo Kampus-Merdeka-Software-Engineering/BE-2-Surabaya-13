543ca8789c965be46e31d0e5999121e6c3c75b24
Yosshhh <yosuahutasoit082@gmail.com>
2023-11-30 23:18:14 +0700

    add crud

--- controllers/hotel-controllers.js
+++ controllers/hotel-controllers.js
@@ -21,4 +21,36 @@ const addHotels = async (req, res) => {
   }
 };
 
-module.exports = { getHotels, addHotels };
+const updateHotel = async (req, res) => {
+  try {
+    const updatedHotel = await HotelModel.update(req.body, { where: { id: req.params.id } });
+    
+    if (updatedHotel[0] === 1) {
+      res.status(200).json({ message: 'Hotel updated successfully' });
+    } else {
+      res.status(404).json({ message: 'Hotel not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to update hotel' });
+  }
+};
+
+const deleteHotel = async (req, res) => {
+  try {
+    const deletedHotel = await HotelModel.destroy({ where: { id: req.params.id } });
+    
+    if (deletedHotel === 1) {
+      res.status(200).json({ message: 'Hotel deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Hotel not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to delete hotel' });
+  }
+};
+
+
+
+module.exports = { getHotels, addHotels, updateHotel, deleteHotel };
--- router/router-hotel.js
+++ router/router-hotel.js
@@ -4,5 +4,7 @@ const hotelController = require('../controllers/hotel-controllers');
 
 router.get('/hotel', hotelController.getHotels);
 router.post('/hotel', hotelController.addHotels);
+router.put('/hotel', hotelController.updateHotel);
+router.delete('/hotel', hotelController.addHotels);
 
 module.exports = router;
