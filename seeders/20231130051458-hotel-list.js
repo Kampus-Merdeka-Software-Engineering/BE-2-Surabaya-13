522ac299d15c38c661465c70575b34b5ccd2e003
Yosshhh <yosuahutasoit082@gmail.com>
2023-12-03 17:27:45 +0700

    changes database to railway

--- seeders/20231130051458-hotel-list.js
+++ seeders/20231130051458-hotel-list.js
@@ -7,18 +7,24 @@ module.exports = {
     await queryInterface.bulkInsert('hotel', [{
       Name: 'Badung, Indonesia',
       Description: 'Book your gateway to this contem..',
-      Price: 'Rp16,161,062 night'
+      Price: 'Rp16,161,062 night',
+      createdAt: new Date(),
+      updatedAt: new Date()
       },
 
       {
       Name: 'Canggu, Indonesia',
       Description: 'Book your gateway to this contem..',
-      Price: 'Rp24,644,818 night'
+      Price: 'Rp24,644,818 night',
+      createdAt: new Date(),
+      updatedAt: new Date()
       },
       {
       Name: 'Kutuh, Indonesia',
       Description: 'Book your gateway to this contem..',
-      Price: 'Rp7,329,305 night'
+      Price: 'Rp7,329,305 night',
+      createdAt: new Date(),
+      updatedAt: new Date()
       }
 
     ], {});
