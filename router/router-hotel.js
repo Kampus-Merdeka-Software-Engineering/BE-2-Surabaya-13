c0221eac300f3a225f3966572ae1ef9c3744499d
Yosshhh <yosuahutasoit082@gmail.com>
2023-12-04 18:12:42 +0700

    changes router

--- router/router-hotel.js
+++ router/router-hotel.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const hotelController = require('../controllers/hotel-controllers');
 
 router.get('/hotels', hotelController.getHotels);
-router.get('/hotelsId', hotelController.getHotelsbyID);
+router.get('/hotels/detailId', hotelController.getHotelsbyID);
 router.post('/hotel', hotelController.addHotels);
 router.put('/hotelId', hotelController.updateHotel);
-router.delete('/hotel/:hotelId', hotelController.addHotels);
+router.delete('/hotel/:hotelId', hotelController.deleteHotel);
 
 module.exports = router;
