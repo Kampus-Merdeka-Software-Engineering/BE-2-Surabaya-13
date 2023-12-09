597ae6c123d678e58c88f15b70fecb0c7ac0ee7c
Yosshhh <yosuahutasoit082@gmail.com>
2023-12-09 13:51:10 +0700

    rename routes

--- router/router-reserved.js
+++ router/router-reserved.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const reserveController = require('../controllers/reserved-controllers');
 
-router.post('/reserve', reserveController.Reserve);
-router.get('/reserves', reserveController.getReserve);
+router.post('/reserved', reserveController.Reserve);
+router.get('/reserveds', reserveController.getReserve);
 
 module.exports = router;
